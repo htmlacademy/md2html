d93bf73ee5ac2632d472283f2eccf76eb5687c7b
Slava <slava.milin@gmail.com>
2019-04-01 15:00:28 +0300

    add frame container

--- src/containers/ResultWrap.js
+++ src/containers/ResultWrap.js
@@ -1,9 +1,32 @@
-import ResultWrap from "../components/ResultWrap";
 import { connect } from "react-redux";
+import React, { Component } from "react";
+import ResultCode from "../components/ResultCode";
+import mdConverter from "../utils/mdConverter";
+import { Tabs } from "../constants";
+import ResultFrame from "../components/ResultFrame";
 
 const mapStateToProps = state => ({
   text: state.text,
   currentTab: state.currentTab
 });
+
+class ResultWrap extends Component {
+  state = {
+    [Tabs.code]: ResultCode,
+    [Tabs.frame]: ResultFrame
+  };
+
+  render() {
+    let { text, currentTab } = this.props;
+    text = mdConverter(text);
+    const CurrentElement = this.state[currentTab];
+
+    return (
+      <React.Fragment>
+        <CurrentElement text={text} />
+      </React.Fragment>
+    );
+  }
+}
 
 export default connect(mapStateToProps)(ResultWrap);
