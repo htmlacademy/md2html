dc3567f4a5dfbc3c8a9eb2a7ea1cac563cb53f40
Slava <slava.milin@gmail.com>
2019-04-01 15:08:02 +0300

    Updates

---
+++ precache-manifest.6d961a67b6c7c8713d74457fe8c8bc07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "612285f46f85a69de424",
+    "url": "/md2html/static/js/runtime~main.f9797ad8.js"
+  },
+  {
+    "revision": "1b650acd0fb4641f4103",
+    "url": "/md2html/static/js/main.f003f113.chunk.js"
+  },
+  {
+    "revision": "7faa723787ac2c45637f",
+    "url": "/md2html/static/js/3.26788ae0.chunk.js"
+  },
+  {
+    "revision": "1ca4f24d82eb0e2dcc55",
+    "url": "/md2html/static/js/2.4b3e5625.chunk.js"
+  },
+  {
+    "revision": "1ca4f24d82eb0e2dcc55",
+    "url": "/md2html/static/css/2.ce002770.chunk.css"
+  },
+  {
+    "revision": "ce83ba7cb445fbcd74126a1874fa8727",
+    "url": "/md2html/index.html"
+  },
+  {
+    "revision": "95da5e9fe4db3b7ebfa2a179b97cac8d",
+    "url": "/md2html/editor.worker.js"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/md2html/precache-manifest.6d961a67b6c7c8713d74457fe8c8bc07.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/md2html/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
