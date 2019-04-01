65a49b15a0ae42cd098b3b5cebb8bb2529053256
Slava <slava.milin@gmail.com>
2019-04-01 15:00:10 +0300

    change default text

--- src/reducers/editor.js
+++ src/reducers/editor.js
@@ -2,10 +2,32 @@ import { createReducer } from "redux-starter-kit";
 import { changeTab, changeText } from "../actions";
 import { Tabs } from "../constants";
 
+const defaultText = `# Заголовок
+
+- Список
+  1. Вложенный список
+
+      \`\`\`html
+      <div>Вложенная вёрстка</div>
+      \`\`\`
+
+      \`\`\`js
+      // Вложенный JS
+      const sum = (a, b) => {
+        return a + b;
+      }
+      \`\`\`
+  2. Второй элемент
+
+###big-trip
+
+Выше - разделитель для заданий. Итоговый код прогоняется через типограф, поэтому пробелы и тире в тексте правильные. Также готовый HTML код прогоняется через бьютифаер, чтобы не было каши.
+`;
+
 const editorReducer = createReducer(
   {
     text: "",
-    defaultText: "# Тестовый заголовок",
+    defaultText,
     currentTab: Tabs.code
   },
   {
