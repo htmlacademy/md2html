a6ba9930370501c8d93fb6e9eb2f22f046064eac
Slava <slava.milin@gmail.com>
2019-04-16 13:56:34 +0300

    add header id action and refactor constants

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,13 @@
 import { createAction } from "redux-starter-kit";
 import mdConverter from "../utils/mdConverter";
-import { CONVERT_HLJS, HEADER_LEVEL } from "../constants";
+import { ADD_HEADER_ID, CONVERT_HLJS, HEADER_LEVEL } from "../constants";
 
 export const setMarkup = createAction("CHANGE_TEXT");
 export const changeTab = createAction("CHANGE_TAB");
 export const toggleShowSettings = createAction("TOGGLE_SHOW_SETTINGS");
-export const convertHljs = createAction("CONVERT_HLJS");
 export const changeLevelTitle = createAction("CHANGE_LEVEL_TITLE");
+export const convertHljs = createAction(CONVERT_HLJS);
+export const addHeaderId = createAction(ADD_HEADER_ID);
 
 export const changeSettings = ({ name, value }) => (dispatch, getState) => {
   switch (name) {
@@ -15,6 +16,9 @@ export const changeSettings = ({ name, value }) => (dispatch, getState) => {
       break;
     case HEADER_LEVEL:
       dispatch(changeLevelTitle(value));
+      break;
+    case ADD_HEADER_ID:
+      dispatch(addHeaderId());
       break;
     default:
       break;
