b379e18fb934d6858fc3a59a0cf2b7045ccaff3d
Slava <slava.milin@gmail.com>
2019-04-16 14:41:46 +0300

    add id to link action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,11 @@
 import { createAction } from "redux-starter-kit";
 import mdConverter from "../utils/mdConverter";
-import { ADD_HEADER_ID, CONVERT_HLJS, HEADER_LEVEL } from "../constants";
+import {
+  ADD_HEADER_ID,
+  ADD_ID_TO_LINK,
+  CONVERT_HLJS,
+  HEADER_LEVEL
+} from "../constants";
 
 export const setMarkup = createAction("CHANGE_TEXT");
 export const changeTab = createAction("CHANGE_TAB");
@@ -8,6 +13,7 @@ export const toggleShowSettings = createAction("TOGGLE_SHOW_SETTINGS");
 export const changeLevelTitle = createAction("CHANGE_LEVEL_TITLE");
 export const convertHljs = createAction(CONVERT_HLJS);
 export const addHeaderId = createAction(ADD_HEADER_ID);
+export const addIdToLink = createAction(ADD_ID_TO_LINK);
 
 export const changeSettings = ({ name, value }) => (dispatch, getState) => {
   switch (name) {
@@ -19,6 +25,9 @@ export const changeSettings = ({ name, value }) => (dispatch, getState) => {
       break;
     case ADD_HEADER_ID:
       dispatch(addHeaderId());
+      break;
+    case ADD_ID_TO_LINK:
+      dispatch(addIdToLink());
       break;
     default:
       break;
