6d2fea6a73455856d1fd63052c0ad50afecb4540
Slava <slava.milin@gmail.com>
2019-04-16 13:28:04 +0300

    add header level constant and moove default text

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,3 +3,26 @@ export const Tabs = {
   frame: "Предпросмотр"
 };
 export const CONVERT_HLJS = "CONVERT_HLJS";
+export const HEADER_LEVEL = "HEADER_LEVEL";
+
+export const defaultText = `# Заголовок
+
+- Список
+  1. Вложенный список
+
+      \`\`\`html
+      <div>Вложенная вёрстка</div>
+      \`\`\`
+
+      \`\`\`js
+      // Вложенный JS
+      const sum = (a, b) => {
+        return a + b;
+      }
+      \`\`\`
+  2. Второй элемент
+
+###big-trip
+
+Выше - разделитель для заданий. Итоговый код прогоняется через типограф, поэтому пробелы и тире в тексте правильные. Также готовый HTML код прогоняется через бьютифаер, чтобы не было каши.
+`;
