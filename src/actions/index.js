0b5571031d480fa6301db35d2ab6541121cb35cd
Slava <slava.milin@gmail.com>
2019-04-16 12:52:09 +0300

    add actions for settings

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,31 @@
 import { createAction } from "redux-starter-kit";
 import mdConverter from "../utils/mdConverter";
+import { CONVERT_HLJS } from "../constants";
 
 export const setMarkup = createAction("CHANGE_TEXT");
 export const changeTab = createAction("CHANGE_TAB");
 export const toggleShowSettings = createAction("TOGGLE_SHOW_SETTINGS");
-export const toggleHljsSettings = createAction("TOGGLE_HLJS_ACTION");
+export const convertHljs = createAction("CONVERT_HLJS");
 
-export const changeText = text => dispatch =>
-  dispatch(setMarkup(mdConverter(text)));
+export const changeSettings = name => (dispatch, getState) => {
+  switch (name) {
+    case CONVERT_HLJS:
+      dispatch(convertHljs());
+      break;
+    default:
+      break;
+  }
+
+  const { rawText } = getState();
+
+  dispatch(changeText(rawText));
+};
+
+export const changeText = text => (dispatch, getState) => {
+  const { convertHljs } = getState();
+  const settings = {
+    convertHljs
+  };
+  dispatch({ type: "SAVE_RAW_TEXT", payload: text });
+  dispatch(setMarkup(mdConverter(text, settings)));
+};
